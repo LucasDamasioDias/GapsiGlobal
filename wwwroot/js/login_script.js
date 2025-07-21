78cdabe3bd46b7c06808275c86f8fec04d6044c9
Lucas Damásio <132855392+LucasDamasioDias@users.noreply.github.com>
2025-07-21 16:59:38 -0300

    Inclusão de script do Login

---
+++ wwwroot/js/login_script.js
@@ -0,0 +1,10 @@
+﻿document.addEventListener('DOMContentLoaded', function () {
+    const senhaInput = document.getElementById('Senha');
+    const mostrarSenhaCheckbox = document.getElementById('mostrarSenhaCheckbox');
+
+    if (senhaInput && mostrarSenhaCheckbox) {
+        mostrarSenhaCheckbox.addEventListener('change', function () {
+            senhaInput.type = this.checked ? 'text' : 'password';
+        });
+    }
+})
